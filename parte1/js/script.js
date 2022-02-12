f82695cd95dca1f4d91f1699d8dd2e26b79c07f8
LRiiic <ricardo_santos-2010@hotmail.com>
2022-02-11 21:54:01 -0300

    fixing mobile and desktop header

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,7 @@
-/* Coloque o JS neste arquivo */
+function openButton() {
+    var menu = document.getElementsByClassName('menu-mobile')[0].style.display = "flex";
+}
+
+function closeButton() {
+    var menu = document.getElementsByClassName('menu-mobile')[0].style.display = "none";
+}
