083f1f41497647c4517df28cbf4fce9fe0eb9806
LRiiic <ricardo_santos-2010@hotmail.com>
2022-02-12 18:40:10 -0300

    add fixed buttons and create cart count

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -5,3 +5,8 @@ function openButton() {
 function closeButton() {
     var menu = document.getElementsByClassName('menu-mobile')[0].style.display = "none";
 }
+
+function gotoTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
